3ee26670e9fef8586818110a6da10009c23a07e8
Fareed Dudhia <fareeddudhia@gmail.com>
2014-12-06 08:06:50 -0500

    Fix image tags

--- recruit/tests/models.py
+++ recruit/tests/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class TestImage(models.Model):
     label = models.CharField(max_length=100, default='')
-    image = models.CharField(max_length=100, default='ubuntu:latest')
+    image = models.CharField(max_length=100, default='ubuntu')
